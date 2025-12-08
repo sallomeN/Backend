c3c56c393827e4e65290a565556ddef429dc46df
sallomeN <nozadzesalome763@gmail.com>
2025-12-08 17:26:38 +0400

    fixed authentication

--- routes/laptop.routes.js
+++ routes/laptop.routes.js
@@ -4,13 +4,17 @@ import Laptop from "../models/Laptop.model.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const laptops = await Laptop.find();
-  res.json(laptops);
+  try {
+    const laptops = await Laptop.find();
+    res.status(200).json(laptops);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    const laptop = await LaptopModel.findById(req.params.id);
+    const laptop = await Laptop.findById(req.params.id);
     if (!laptop) return res.status(404).json({ message: "Not found" });
     res.json(laptop);
   } catch (err) {
--- routes/phone.routes.js
+++ routes/phone.routes.js
@@ -4,13 +4,17 @@ import Phone from "../models/Phone.model.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const phones = await Phone.find();
-  res.json(phones);
+  try {
+    const phones = await Phone.find();
+    res.status(200).json(phones);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    const phone = await PhoneModel.findById(req.params.id);
+    const phone = await Phone.findById(req.params.id);
     if (!phone) return res.status(404).json({ message: "Not found" });
     res.json(phone);
   } catch (err) {
